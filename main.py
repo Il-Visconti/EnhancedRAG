d8c2927f2deb69e956d4a760d3892dce7144351b
Il-Visconti <el.duarte.tomas@gmail.com>
2025-07-15 16:06:44 +0100

    Added both Graph and Vector RAG prompt search as well as a prompt template.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from VectorRAG import query_vector_rag
 from GraphRAG import generate_cypher_query
 graph, openAI_api, openAI_endpoint = load_neo4j_graph()
 
-question = "who killed napoleon?"
+question = "where did napoleon die?"
 vector_index_name = 'Chunk'
 vector_node_label = 'Chunk'
 vector_source_property= 'text'
@@ -18,5 +18,5 @@ Vector_RAG = query_vector_rag(
     )
 print(Vector_RAG)
 
-generate_cypher_query(graph=graph, question=question)
-
+GraphRAG = generate_cypher_query(graph=graph, question=question)
+print(GraphRAG)
